eaf801f2be2fc2495099eaa75e2cbbb6cdc778d5
Eric Johnson <ejohn156@uncc.edu>
2018-06-17 21:12:35 -0400

    finished majority

--- script.js
+++ script.js
@@ -1,112 +1,98 @@
 //initialize database
 var rowColor = ""
 var rowCounter = 0;
-var currentDateYear = moment().format("YYYY")
-var currentDateMonth = moment().format("MM")
-
+var currentTime = moment().format("HH:mm")
+var startTime;
 var config = {
-    apiKey: "AIzaSyBZeMQmksdsqPdOHEXNOST7amL6j6vZ-24",
-    authDomain: "timesheet-8ce5d.firebaseapp.com",
-    databaseURL: "https://timesheet-8ce5d.firebaseio.com",
-    projectId: "timesheet-8ce5d",
+    apiKey: "AIzaSyDPyJxS9RO45emWQqVAL6ILn8gxpk0x5-I",
+    authDomain: "trainarrivals-4a16e.firebaseapp.com",
+    databaseURL: "https://trainarrivals-4a16e.firebaseio.com",
+    projectId: "trainarrivals-4a16e",
     storageBucket: "",
-    messagingSenderId: "680559553665"
+    messagingSenderId: "838817394324"
 };
 firebase.initializeApp(config);
 database = firebase.database();
 
 //database action once child is added to employee data folder
-database.ref("/employeeData").on("child_added", function (childSnapshot) {
+database.ref("/trainData").on("child_added", function (childSnapshot) {
     rowCounter++;
     var newRow = $("<tr>")
-    newRow.attr("id", childSnapshot.val().employeeID)
+    newRow.attr("id", childSnapshot.val().trainID)
 
-    var delButton = $("<button>")
-    delButton.html("X")
-    delButton.addClass("delete")
-    delButton.attr("value", childSnapshot.val().employeeID)
     var nameCell = $("<td>")
-    var roleCell = $("<td>")
-    var sdCell = $("<td>")
+    var destCell = $("<td>")
+    var freqCell = $("<td>")
     var mwCell = $("<td>")
-    var mrCell = $("<td>")
-    var tbCell = $("<td>")
-    var delCell = $("<td>")
-    delCell.append(delButton)
+    var nextCell = $("<td>")
+    var awayCell = $("<td>")
     nameCell.text(childSnapshot.val().name)
-    roleCell.text(childSnapshot.val().role)
-    sdCell.text(childSnapshot.val().startDate)
-    mwCell.text(childSnapshot.val().monthsWorked)
-    mrCell.text(childSnapshot.val().monthlyRate)
-    tbCell.text(childSnapshot.val().totalBilled)
+    destCell.text(childSnapshot.val().destination)
+    freqCell.text(childSnapshot.val().frequency)
+    nextCell.text(childSnapshot.val().arrivalTime)
+    awayCell.text(childSnapshot.val().minAway)
     $(newRow).append(nameCell)
-    $(newRow).append(roleCell)
-    $(newRow).append(sdCell)
-    $(newRow).append(mwCell)
-    $(newRow).append(mrCell)
-    $(newRow).append(tbCell)
-    $(newRow).append(delButton)
-    $("#employeeRow").append(newRow)
-    $("#employeeRow").css("background-color", rowColor)
+    $(newRow).append(destCell)
+    $(newRow).append(freqCell)
+    $(newRow).append(nextCell)
+    $(newRow).append(awayCell)
+
+
+    $("#trainRow").append(newRow)
+    $("#trainRow").css("background-color", rowColor)
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
 //function that occurs once the submit button of the form is clicked
-$("#add-emp").on("click", function (event) {
+$("#add-train").on("click", function (event) {
     event.preventDefault()
-    
-    
+
+
     //gather info from form
     var name = $("#name-input").val().trim()
-    var role = $("#role-input").val().trim()
-    var startDate = $("#sd-input").val().trim()
-    startDate = moment(startDate).format("MM/DD/YYYY")
-    var monthsWorked = findMonthsWorked(startDate);
-    var monthlyRate = $("#rate-input").val().trim()
-    var totalBilled = monthsWorked * monthlyRate
-    //fill db with values
-    database.ref("/employeeData").push({
+    var destination = $("#dest-input").val().trim()
+    startTime = $("#first-input").val().trim()
+    startTime = moment(startTime, "HH:mm a")
+    
+    
+    var frequency = $("#freq-input").val().trim()
+    
+    var minAway = nextArrival(startTime, frequency)
+    var arrivalTime = findNextArrivalTime(minAway)
+    database.ref("/trainData").push({
         name: name,
-        role: role,
-        startDate: startDate,
-        monthsWorked: monthsWorked,
-        monthlyRate: monthlyRate,
-        totalBilled: totalBilled,
-        employeeID: "emp-" + (rowCounter + 1)
+        destination: destination,
+        frequency: frequency,
+        arrivalTime: arrivalTime,
+        minAway: minAway,
+        trainID: "train-" + (rowCounter + 1)
     });
     //clears form input fields
     $("#name-input").val("")
-    $("#role-input").val("")
-    $("#rate-input").val("")
-    $("#sd-input").val("")
+    $("#dest-input").val("")
+    $("#freq-input").val("")
+    $("#first-input").val("")
 
 });
 
-if (rowCounter % 2 == 0){
+if (rowCounter % 2 == 0) {
     rowColor = "lightblue"
 }
 
-    
-function findMonthsWorked(startDate){
-    var monthsWorked;
-    var yearsWorked;
-    
-    var startMonth = moment(startDate).format("MM")
-    var startYear = moment(startDate).format("YYYY")
-    
-    monthsWorked = (parseInt(currentDateYear) - parseInt(startYear)) * 12
-    
-    monthsWorked += (parseInt(currentDateMonth) - parseInt(startMonth))
-    
-    return monthsWorked
+
+function findNextArrivalTime(minAway) {
+    var nextTrain = moment().add(minAway, "minutes")
+    return moment(nextTrain).format("hh:mm")
 }
 
-$(document).on("click", ".delete", function(event){
-    event.preventDefault()
-    alert("delete button clicked")
-    var trow = $(this).attr("value")
-    alert(trow)
-    $("#" + trow).empty()
+function nextArrival(startTime, frequency){
+    var nextArrival = totalTime(startTime) % frequency
+    return nextArrival
+}
 
+function totalTime(startTime) {
 
-})
+    var mins = moment().diff(moment(startTime), "minutes");
+    return mins
+
+}
